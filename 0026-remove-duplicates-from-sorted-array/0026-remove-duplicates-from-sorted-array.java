67df9f91433d5ce63dcebba14465240f0091ef27
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-10-19 19:08:14 +0530

    Time: 3 ms (15.83%), Space: 48.2 MB (13.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        //Use as a frequency element 
          int n = nums.length;
         int[] arr = new int[n];
         int m = Integer.MIN_VALUE;
-        int  jdx = 0;
+        int  j = 0;//Index of next array 
         for(int i = 0; i < n; i++){
             if(nums[i] >m){
                 m = nums[i];
-                arr[jdx] = m;
-                jdx++;
+                arr[j] = m;
+                j++;
             }   
         }
         for(int i = 0; i< arr.length; i++)
             nums[i] = arr[i];
         
-        return jdx ;
+        return j ;
     }
 }
