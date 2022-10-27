a0b66c3b6612b012f0a2495f280eae4e3f967b63
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-10-27 22:28:44 +0530

    Time: 5 ms (51.04%), Space: 40.6 MB (71.49%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -7,7 +7,8 @@ class Solution {
         int sum = 1;
         for(int i = 2; i*i <num; i++){
             if(num % i == 0){
-                sum += i + num/i;
+                sum += i;
+                sum+=num/i;
             }
         }
         
