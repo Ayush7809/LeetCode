ed4428260b51c2b644d34d52d640735c13a12ce6
Ayush Dubey <104785802+Ayush7809@users.noreply.github.com>
2026-01-01 17:45:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (19.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,12 +3,12 @@ class Solution {
         int n = digits.length;
         for(int i=n-1;i>=0;i--){
             if(digits[i]<9){
-                 digits[i]++;
+                digits[i]++;
                 return digits;
             }
             digits[i]=0;
         }
-        int [] arr = new int[n+1];
+        int [] arr = new int [n+1];
         arr[0]=1;
         return arr;
     }
