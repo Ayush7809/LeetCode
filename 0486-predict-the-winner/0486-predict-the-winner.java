cb55f0e111e867fb61f74716e1a6f19405e532c5
Ayush Dubey <104785802+Ayush7809@users.noreply.github.com>
2026-04-22 11:16:08 +0530

    Time: 0 ms (100%), Space: 42.6 MB (75.87%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        return winner(nums, 0, nums.length-1)>=0;
+        int [][] dp = new int[nums.length][nums.length];
+        for(int [] a : dp){
+            Arrays.fill(a, -1);
+        }
+        return winner(nums, 0, nums.length-1, dp)>=0;
     }
-    public static int winner(int [] nums, int i, int j){
+    public static int winner(int [] nums, int i, int j, int [][] dp){
         int n = nums.length;
         if(i==j)
             return nums[i];
-        int left = nums[i] - winner(nums, i+1,j);
-        int right = nums[j] - winner(nums, i, j-1);
-        return Math.max(left, right);
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int left = nums[i] - winner(nums, i+1,j,dp);
+        int right = nums[j] - winner(nums, i, j-1,dp);
+        return dp[i][j] = Math.max(left, right);
     }
 }
