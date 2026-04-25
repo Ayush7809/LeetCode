b59423a5335a742ec0223b436312829bbdb4a60d
Ayush Dubey <104785802+Ayush7809@users.noreply.github.com>
2026-04-25 17:29:10 +0530

    Time: 1 ms (22.95%), Space: 44 MB (28.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,20 +1,56 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i,j,k=nums1.length-1;
-        for(i=m-1,j=n-1; i>=0 && j>=0;)
-        {
-        if(nums1[i]>=nums2[j])
-        {
-        nums1[k--]=nums1[i--];
+    public void merge(int[] arr, int n, int[] arr1, int m) {
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // int i=0, j=0;
+        // while(i<m && j<n){
+        //     if(nums1[i]<nums2[j]){
+        //         arr.add(nums1[i]);
+        //         i++;
+        //     }
+        //     else{
+        //         arr.add(nums2[j]);
+        //         j++;
+        //     }
+        // }
+        // while(i<m){
+        //     arr.add(nums1[i]);
+        //     i++;
+        // }
+        // while(j<n){
+        //     arr.add(nums2[j]);
+        //     j++;
+        // }
+        // for(i=0;i<arr.size();i++){
+        //     nums1[i] = arr.get(i);
+        // }
+
+
+        // for(int j=0,i=m;j<n;j++){
+        //     nums1[i] = nums2[j];
+        //     i++;
+        // }
+        // Arrays.sort(nums1);
+
+        
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(arr[i]<arr1[j]){
+                arr2.add(arr[i++]);
+                
+            }
+            else{
+                arr2.add(arr1[j++]);
+            }
         }
-        else 
-        {
-        nums1[k--]=nums2[j--];
+        while(i<n){
+            arr2.add(arr[i++]);
         }
+        while(j<m){
+            arr2.add(arr1[j++]);
         }
-        while(j>=0 && i<0)
-        {
-        nums1[k--]=nums2[j--];
-        }
+        for(i=0;i<arr2.size();i++){
+            arr[i] = arr2.get(i);
+         }
     }
 }
